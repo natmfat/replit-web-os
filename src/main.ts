6127bd70bdd06941ccecd4417de1e46f5e983293
Nathan Pham <nathanpham.me@gmail.com>
2022-07-21 00:21:26 -0700

    add all icons

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,31 @@
 import "@/css/globals.css";
 import "@/css/index.css";
+
+import { apps } from "@/ts/apps";
+
+const appsContainer = document.querySelector(".apps")!;
+
+// render apps
+for (const app of apps) {
+    // create app
+    const appElement = document.createElement("div");
+    appElement.className = "app";
+
+    // create app name
+    const appTitle = document.createElement("span");
+    appTitle.textContent = app.name;
+    appTitle.className = "app__title";
+
+    // apply background color
+    if (app.bg) {
+        appElement.style.background = app.bg;
+    }
+
+    // add icon
+    if (app.icon) {
+        appElement.innerHTML = app.icon;
+    }
+
+    appElement.appendChild(appTitle);
+    appsContainer.appendChild(appElement);
+}
---
+++ src/ts/apps.ts
@@ -0,0 +1,63 @@
+import * as icons from "./icons";
+
+export const apps = [
+    {
+        name: "Home",
+        bg: "#ffffff",
+        icon: icons.home,
+    },
+    {
+        name: "Templates",
+        bg: "#d3483f",
+        icon: icons.templates,
+    },
+    {
+        name: "My Repls",
+        bg: "#5782ea",
+        icon: icons.folder,
+    },
+    {
+        name: "Learn",
+        bg: "#8a4f4d",
+        icon: icons.apple,
+    },
+    {
+        name: "Community",
+        bg: "#67a3d6",
+        icon: icons.web,
+    },
+    {
+        name: "Teams",
+        bg: "#78c84f",
+        icon: icons.teams,
+    },
+    {
+        name: "Curriculum",
+        bg: "#da7f37",
+        icon: icons.book,
+    },
+    {
+        name: "Languages",
+        apps: [{}],
+    },
+    {
+        name: "Settings",
+        bg: "#1d2334",
+        icon: icons.pen,
+    },
+    {
+        name: "Create Repl",
+        bg: "#2b54a8",
+        icon: icons.add,
+    },
+    {
+        name: "Upgrade",
+        bg: "#355f1e",
+        icon: icons.rocket,
+    },
+    {
+        name: "Notifications",
+        bg: "#960711",
+        icon: icons.bell,
+    },
+];
